15092cfbf95aa4aa186a9d46b39ee4b29ac6139c
Boris Vedernikov <vedernikov.boris@gmail.com>
2023-03-02 22:13:05 +0200

    start now button

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,8 @@
 <template style="background-color: black">
-<!--  <NavDrawer></NavDrawer>-->
-<!--  <PicSlider></PicSlider>-->
   <v-container fluid style="padding: 0 ; margin: 0">
 
-<!--    <v-img src="https://cdn.discordapp.com/attachments/933565701162168371/1053345367929913415/Romarioagrow_design_of_web_site_with_unreal_engine_space_simula_8e6308c2-4769-457f-a7f9-36b8e6b29e23.png"/>-->
-<!--    <v-img src="C:\Projects\aps_frontend_vue\src\assets\main-pic-gigapixel.png"/>-->
-    <v-img :src="require('../assets/grid_0 (3d) - Copy-gigapixel-art-scale-2_00x.png')" style="opacity: 70%;" >
+    <v-img :src="require('../assets/grid_0 (3d) - Copy-gigapixel-art-scale-2_00x.png')" style="opacity: 70%;">
 
-<!--      <v-btn style="margin-top: 25%">LEL</v-btn>&ndash;&gt;-->
 
       <v-container justify="center" >
         <v-row justify="center" align="center">
@@ -27,8 +22,8 @@
                 Spaceship trips. Colonies and Spaceports. Anomalies and Exploring.</v-card-text> </v-card-subtitle>
 
               <v-card-actions>
-                <v-btn variant="text"  block>
-                  Start Now
+                <v-btn :rounded="0" height="100%" color="secondary" @click="goToView('/start')" style="font-family: 'Osiris'" >
+                  START NOW
                 </v-btn>
               </v-card-actions>
             </v-card>
@@ -48,12 +43,17 @@
 
 
 <script>
+import router from "@/router";
 /* eslint-disable */
-import NavDrawer from "@/components/NavBar";
 import PicSlider from "@/components/PicSlider";
 export default {
   name: "MainPage",
-  components: {PicSlider}
+  components: {PicSlider},
+  methods: {
+    goToView(view_url) {
+      router.push(view_url)
+    }
+  },
 }
 </script>
 
